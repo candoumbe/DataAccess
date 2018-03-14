aeabcb964d9ef5894c6f2c891f44826fc3172912
Cyrille NDOUMBE <ndoumbecyrille@hotmail.com>
2018-03-14 16:33:56 +0100

    Startup code refactoring

--- src/tools/MedEasy.DAL/Interfaces/UnitOfWorkFactory.cs
+++ src/tools/MedEasy.DAL/Interfaces/UnitOfWorkFactory.cs
@@ -1,7 +1,14 @@
 namespace MedEasy.DAL.Interfaces
 {
+    /// <summary>
+    /// Factory to create <see cref="IUnitOfWork"/>s.
+    /// </summary>
     public abstract class UnitOfWorkFactory : IUnitOfWorkFactory
     {
+        /// <summary>
+        /// Starts a new unit of work
+        /// </summary>
+        /// <returns></returns>
         public abstract IUnitOfWork NewUnitOfWork();
     }
 }
